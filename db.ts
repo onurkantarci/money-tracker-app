6f333712a50b1889aaaf396739839d29e0f1e892
Onur Kantarci <onurkntrc@gmail.com>
2024-03-13 13:01:51 +0300

    feat: create new db

---
+++ db.ts
@@ -0,0 +1,21 @@
+import Dexie, { Table } from "dexie";
+
+export interface Transaction {
+  id: number;
+  description: string;
+  amount: number;
+  date: Date;
+}
+
+export class MyMoneyTrackingDexie extends Dexie {
+  Transactions!: Table<Transaction>;
+
+  constructor() {
+    super("moneyTrackingDB");
+    this.version(1).stores({
+      transactions: "++id, description, amount, date",
+    });
+  }
+}
+
+export const db = new MyMoneyTrackingDexie();
