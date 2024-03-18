ff2c36fa411836894e5f90ae6b410526d98e5852
Onur Kantarci <onurkntrc@gmail.com>
2024-03-18 16:16:02 +0300

    feat: add categories path

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,9 +10,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/About.vue"),
+      path: "/categories",
+      name: "categories",
+      component: () => import("../views/Categories.vue"),
     },
   ],
 });
