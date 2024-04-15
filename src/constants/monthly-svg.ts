24e2036514a4584c6fade9ae5602c9812842aa62
Onur Kantarci <onurkntrc@gmail.com>
2024-04-15 16:17:05 +0300

    feat: create monthly-svg.ts for month-selector's background

---
+++ src/constants/monthly-svg.ts
@@ -0,0 +1,22 @@
+export const monthlySvgs = [
+  {
+    season: "winter",
+    months: [11, 0, 1],
+    svg: "winter.svg",
+  },
+  {
+    season: "spring",
+    months: [2, 3, 4],
+    svg: "spring.svg",
+  },
+  {
+    season: "summer",
+    months: [5, 6, 7],
+    svg: "summer.svg",
+  },
+  {
+    season: "fall",
+    months: [8, 9, 10],
+    svg: "fall.svg",
+  },
+];
