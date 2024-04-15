0c17f00c07b2a06b9a9bd9ecffb15c2bc1ecd6c1
Onur Kantarci <onurkntrc@gmail.com>
2024-04-15 16:17:31 +0300

    refactor: adjust the categories view

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,81 +1,44 @@
 <template>
-  <div>
-    <input
-      type="radio"
-      name="categoryType"
-      v-model="selectedOption"
-      value="income"
-    />
-    <label for="income">Income</label>
+  <div class="categories-container">
+    <CategoryList>
+      <v-btn
+        variant="elevated"
+        color="#56CFE1"
+        icon="mdi-plus"
+        class="add-category-button"
+        @click="showAddCategoryDialog()"
+      ></v-btn>
+    </CategoryList>
   </div>
-  <div>
-    <input
-      type="radio"
-      name="categoryType"
-      v-model="selectedOption"
-      value="expense"
-    />
-    <label for="expense">Expense</label>
-  </div>
-  <div class="name-input">
-    <label for="name">Name</label>
-    <input type="text" v-model="categoriesName" />
-  </div>
-  <button @click="saveCategory">Save Category</button>
 
-  <RevenueTable />
+  <CategoryDialog ref="categoryDialog" />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { db } from "../../db";
-import RevenueTable from "../components/RevenueTable.vue";
+import CategoryDialog from "../components/CategoryDialog.vue";
+import CategoryList from "../components/CategoryList.vue";
 
-const categoriesName = ref<string>("");
-const selectedOption = ref<string>("income");
+const categoryDialog = ref<typeof CategoryDialog | null>(null);
 
-async function saveCategory() {
-  console.log({ categoryType: selectedOption.value });
-  try {
-    const id = await db.categories.add({
-      name: categoriesName.value,
-      type: selectedOption.value,
-    });
-    console.log(`Category saved with ID: ${id}`);
-  } catch (error) {
-    console.error(`Error saving category: ${error}`);
-  }
+function showAddCategoryDialog() {
+  categoryDialog.value!.showDialog();
 }
 </script>
 
-<style>
-.name-input {
-  display: flex;
-  flex-direction: column;
-  margin: 10px;
+<style scoped>
+.categories-container {
+  max-width: 650px;
+  position: relative;
+  margin: 0 auto;
+  margin-top: 40px;
 }
 
-.name-input input {
-  max-width: 100px;
-}
-
-.revenue-table {
-  border: 1px solid grey;
-  max-width: 400px;
-  margin: 10px;
-  height: 300px;
-}
-
-.revenue-table button {
-  width: 200px;
-  padding: 10px;
-}
-
-.results {
-  padding: 4px 10px;
-}
-
-.active {
-  background-color: rgb(202, 199, 199);
+.add-category-button {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 58px;
+  margin-right: 110px;
 }
 </style>
