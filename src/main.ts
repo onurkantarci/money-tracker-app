62ab7eebe9f835fdf5d1f106944b41770e2419b8
Onur Kantarci <onurkntrc@gmail.com>
2024-04-15 16:12:37 +0300

    feat: adjust main.ts to be able to use vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,20 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+
+app.use(router);
+
+app.mount("#app");
