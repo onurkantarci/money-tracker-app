38728e1d27bbfad794deea185d77c7362f859423
Onur Kantarci <onurkntrc@gmail.com>
2024-04-21 00:23:47 +0300

    fix: adjust the base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Money-Tracker/",
+  base: "/money-tracker-app/",
   plugins: [vue()],
 });
